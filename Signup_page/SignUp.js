17631aea635baf4e8f5bda3c920461ec0417e990
vishnuiiit <53317018+vishnuiiit@users.noreply.github.com>
2020-03-03 18:32:09 +0530

    Sign Up page updated

--- Signup_page/SignUp.js
+++ Signup_page/SignUp.js
@@ -1,9 +1,12 @@
-function submit(){
-    var user = document.getElementById("username_input");
-    var pass = document.getElementById("password_input");
-    if(user.required && pass.required){
-        window.open("http://localhost:8081/?user=" + user.value + "&pass=" + pass.value, "_self");
-    }
-    else
-        user.value = "notcomplete";
-}
+var error = document.getElementById("error_label");
+// error.innerHTML = "I am here";
+error.innerHTML = "hello";
+var user = document.getElementById("username_input");
+var pass = document.getElementById("password_input");
+var repass = document.getElementById("password_input_1");
+var form = document.getElementById("input_form");
+form.action = "";
+if(pass.value == repass.value)
+    form.action = "http://localhost:8000/";
+else
+    error.innerHTML = "please fill all fields";
--- server_sql.js
+++ server_sql.js
@@ -2,11 +2,17 @@ var http = require("http");
 var mysql = require("mysql");
 var url = require("url");
 var data = require("fs");
+const { parse } = require('querystring');
 var con = mysql.createConnection({
     host: "localhost",
     user: "vishnu",
     password: "",
     database: "account"
+});
+con.connect(function (err){
+    if (err) throw err;
+    console.log("connected");
+    
 });
 http.createServer(function(req, res){
     console.log("hye");
@@ -14,20 +20,15 @@ http.createServer(function(req, res){
     var qdata = q.query;
     console.log("bye");
     console.log(qdata);
-    
-    con.connect(function (err){
-        if (err) throw err;
-        console.log("connected");
-        var sql = "select * from players where username = '" + qdata.user + "' and password = '" + qdata.password + "';";
-        con.query(sql, function(err, result){
-            if(err) throw err;
-            res.writeHead(200, {"Content-type": "text/html"});
-            if(result.length > 0)
-                res.write("Hello World");
-            else
-                res.write("invalid Username or password");
-            res.end();
-        });
+    var sql = "select * from players where username = '" + qdata.user + "' and password = '" + qdata.password + "';";
+    con.query(sql, function(err, result){
+        if(err) throw err;
+        res.writeHead(200, {"Content-type": "text/html"});
+        if(result.length > 0)
+            res.write("Hello World");
+        else
+            res.write("invalid Username or password");
+        res.end();
     });
     console.log("Hye");
 }).listen(8080);
@@ -53,3 +54,39 @@ http.createServer(function(req, res){
         res.end();
     });
 }).listen(8081);
+
+http.createServer(function(req, res){
+    console.log("hey");
+    if (req.method === 'POST') {
+        var body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+            var qdata = parse(body);
+            if(qdata.pass != qdata.repass){
+                res.writeHead(400, {"Content-type" : "text/html"});
+                res.write("password and re-password not matched");
+                res.end(); 
+                return;                                
+            }
+            var sql = "select * from players where username = '" + qdata.user +"';"; 
+            console.log(qdata);
+            con.query(sql, function(err, result){
+                if(err) throw err;
+                res.writeHead(200, {"Content-type" : "text/html"});
+                if(result.length > 0)
+                    res.write("username already used");
+                else{
+                    console.log("here");
+                    var query = "insert into players value ('" + qdata.user + "', '" + qdata.pass + "');";
+                    con.query(query, function(err, result){
+                        if(err) throw err;
+                        console.log("added data successfully");
+                    });
+                }
+                res.end();
+            });
+        });
+    }
+}).listen(8000);
