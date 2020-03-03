8c2bc1a15ae69545c4851378eb460ae5d40de1e5
bhch1507 <56021613+bhch1507@users.noreply.github.com>
2020-03-03 19:12:40 +0530

    stylised button added

--- Login Page/login_button.js
+++ Login Page/login_button.js
@@ -1,3 +1,11 @@
+document.querySelector(".signIn_button").addEventListener("click",function(){
+    // alert("i am pressed");
+    var activeButton=document.querySelector(".signIn_button");
+    document.querySelector(".signIn_button").classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector(".signIn_button").classList.remove("pressed"),150
+    })
+})
 function login(){
     var user = document.getElementById("Username-Input");
     var pass = document.getElementById("Password-Input");
