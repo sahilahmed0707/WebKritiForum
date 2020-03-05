0d8d1eb499e7292cfb825a6ff5ceffa9db25d79d
vishnuiiit <53317018+vishnuiiit@users.noreply.github.com>
2020-03-05 16:19:57 +0530

    added changes to login page and server

--- server_sql.js
+++ server_sql.js
@@ -14,46 +14,32 @@ con.connect(function (err){
     console.log("connected");
     
 });
-http.createServer(function(req, res){
-    console.log("hye");
-    var q = url.parse(req.url, true);
-    var qdata = q.query;
-    console.log("bye");
-    console.log(qdata);
-    var sql = "select * from players where username = '" + qdata.user + "' and password = '" + qdata.password + "';";
-    con.query(sql, function(err, result){
-        if(err) throw err;
-        res.writeHead(200, {"Content-type": "text/html"});
-        if(result.length > 0)
-            res.write("Hello World");
-        else
-            res.write("invalid Username or password");
-        res.end();
-    });
-    console.log("Hye");
-}).listen(8080);
 
 http.createServer(function(req, res){
-    console.log("hye");
-    var q = url.parse(req.url, true);
-    var qdata = q.query;
-    var sql = "select * from players where username = '" + qdata.user +"';"; 
-    con.query(sql, function(err, result){
-        if(err) throw err;
-        res.writeHead(200, {"Content-type" : "text/html"});
-        if(result.length > 0)
-            res.write("username already used");
-        else{
-            console.log("here");
-            var query = "insert into players value ('" + qdata.user + "', '" + qdata.pass + "');";
-            con.query(query, function(err, result){
+    console.log("8080");
+    if(req.method === "POST"){
+        var body = "";
+        console.log("hey");
+        req.on("data", function(chunk){
+            body += chunk.toString();
+        });
+        req.on("end", function(){
+            var qdata = parse(body);
+            console.log(qdata);
+            var sql = "select * from players where username = '" + qdata.user + "' and password = '" + qdata.pass + "';";
+            con.query(sql, function(err, result){
                 if(err) throw err;
-                console.log("added data successfully");
+                res.writeHead(200, {"Content-type": "text/html"});
+                if(result.length > 0)
+                    res.write("Hello World");
+                else
+                    res.write("invalid Username or password");
+                res.end();
             });
-        }
-        res.end();
-    });
-}).listen(8081);
+            console.log("Hye");
+        });
+    }
+}).listen(8080);
 
 http.createServer(function(req, res){
     console.log("hey");
