b85966f0a57e8c13cc5c67e6bf42b9c8e2f415fb
vishnuiiit <53317018+vishnuiiit@users.noreply.github.com>
2020-03-06 16:35:22 +0530

    encrypted, added more details to sign up page

--- server_sql.js
+++ server_sql.js
@@ -21,7 +21,7 @@ app.post("/login", urlencodedParser, function(req, res) {
         user:req.body.user,
         pass:req.body.pass
     };
-    var sql = "select * from players where username = '" + qdata.user + "' and password = '" + qdata.pass + "';";
+    var sql = "select * from players where username = '" + qdata.user + "' and password = aes_encrypt('pass', unhex(sha2('"+ qdata.pass + "', 256)));";
     con.query(sql, function(err, result){
         if(err) throw err;
         if(result.length > 0)
@@ -35,7 +35,11 @@ app.post("/signup", urlencodedParser, function(req, res) {
     var qdata = {
         user: req.body.user,
         pass: req.body.pass,
-        repass: req.body.repass
+        repass: req.body.repass,
+        email: req.body.email,
+        name: req.body.name,
+        ques: req.body.ques,
+        ans: req.body.ans
     };
     if(qdata.pass != qdata.repass){
         res.send("password and re-password not matched");
@@ -50,7 +54,7 @@ app.post("/signup", urlencodedParser, function(req, res) {
             res.send("username already used");
         else{
             console.log("here");
-            var query = "insert into players value ('" + qdata.user + "', '" + qdata.pass + "');";
+            var query = "insert into players values ('"+ qdata.name + "', '" + qdata.email + "', '" + qdata.user + "', aes_encrypt('pass', unhex(sha2('" + qdata.pass + "', 256))), '" + qdata.ques + "', aes_encrypt('ans', unhex(sha2('" + qdata.ans + "', 256))));";
             con.query(query, function(err, result){
                 if(err) throw err;
                 console.log("added data successfully");
