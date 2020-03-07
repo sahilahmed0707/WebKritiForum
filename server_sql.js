b81dcdc8a54c07d99db80c97b8f3d43b908b0fe9
vishnuiiit <53317018+vishnuiiit@users.noreply.github.com>
2020-03-07 22:48:51 +0530

    Added Forgot Password

--- server_sql.js
+++ server_sql.js
@@ -4,11 +4,12 @@ var data = require("fs");
 var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
-var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.set("view engine", "ejs");
 var con = mysql.createConnection({
     host: "localhost",
-    user: "vishnu",
-    password: "",
+    user: "webkriti",
+    password: "12345",
     database: "account"
 });
 con.connect(function (err){
@@ -16,18 +17,116 @@ con.connect(function (err){
     console.log("connected");
 });
 
+app.get("/forgot-password", function(req, res){
+    res.render("ForgotPassword", {
+        "heading": "FORGOT PASSWORD",
+        "subheading": "USERNAME",
+        "input": "user",
+        "display": "initial"
+    });
+});
+app.post("/forgot-password/user", urlencodedParser, function(req, res){
+    var qdata = {
+        user: req.body.user
+    };
+    var sql = "select * from players where username = '" + qdata.user + "';"
+    con.query(sql, function(err, result){
+        if (err) throw err;
+        if(result.length > 0){
+            console.log(result);
+            res.render("ForgotPassword", {
+                "heading": result[0].username,
+                "subheading": result[0].question,
+                "input": "ans",
+                "display": "initial"
+            });
+        }
+        else{
+            res.render("ForgotPassword", {
+                "heading": "nothing",
+                "subheading": "USERNAME DOES NOT EXIST",
+                "input": "nothing",
+                "display": "none"
+            });
+        }
+    });
+});
+
+app.post("/forgot-password/ans", urlencodedParser, function(req, res) {
+    var qdata = {
+        "user": req.body.extra_info,
+        "ans": req.body.ans
+    }
+    var sql = "select * from players where username = '" + qdata.user + "' and ans = aes_encrypt('ans', unhex(sha2('" + qdata.ans + "', 256)));";
+    con.query(sql, function(err, result){
+        if (err) throw err;
+        if(result.length > 0){
+            res.render("NewPassword", {
+                "username": qdata.user
+            });
+        }
+        else{
+            res.render("ForgotPassword", {
+                "heading": "nothing",
+                "subheading": "INCORRECT ANSWER",
+                "input": "nothing",
+                "display": "none"
+            });
+        }
+    })
+});
+
+app.post("/change-password", urlencodedParser, function(req, res){
+    var qdata = {
+        "user": req.body.user,
+        "pass": req.body.pass,
+        "repass": req.body.repass
+    };
+    if(qdata.pass == qdata.repass){
+        var sql = "update players set password = aes_encrypt('" + qdata.pass + "', unhex(sha2('"+ qdata.pass + "', 256))) where username = '" + qdata.user + "';";
+        con.query(sql, function(err, result){
+            if (err) throw err;
+            res.render("ForgotPassword", {
+                "heading": "nothing",
+                "subheading": "PASSWORD CHANGED SUCCESSFULLY",
+                "input": "nothing",
+                "display": "none"
+            });
+        });
+    }
+    else{
+        res.render("ForgotPassword", {
+            "heading": "nothing",
+            "subheading": "PASSWORD AND REPASSWORD DOES NOT MATCH",
+            "input": "nothing",
+            "display": "none"
+        });
+    }
+})
 app.post("/login", urlencodedParser, function(req, res) {
     var qdata = {
         user:req.body.user,
         pass:req.body.pass
     };
-    var sql = "select * from players where username = '" + qdata.user + "' and password = aes_encrypt('pass', unhex(sha2('"+ qdata.pass + "', 256)));";
+    var sql = "select * from players where username = '" + qdata.user + "' and password = aes_encrypt('" + qdata.pass + "', unhex(sha2('"+ qdata.pass + "', 256)));";
     con.query(sql, function(err, result){
         if(err) throw err;
-        if(result.length > 0)
-            res.send("Hello World");
-        else
-            res.send("invalid Username or password");
+        if(result.length > 0){
+            res.render("ForgotPassword", {
+                "heading": "nothing",
+                "subheading": "LOGED IN SUCCESSFULLY",
+                "input": "nothing",
+                "display": "none"
+            });
+        }
+        else{
+            res.render("ForgotPassword", {
+                "heading": "nothing",
+                "subheading": "INVALID USERNAME OR PASSWORD",
+                "input": "nothing",
+                "display": "none"
+            });
+        }
     });
 });
 
@@ -54,7 +153,7 @@ app.post("/signup", urlencodedParser, function(req, res) {
             res.send("username already used");
         else{
             console.log("here");
-            var query = "insert into players values ('"+ qdata.name + "', '" + qdata.email + "', '" + qdata.user + "', aes_encrypt('pass', unhex(sha2('" + qdata.pass + "', 256))), '" + qdata.ques + "', aes_encrypt('ans', unhex(sha2('" + qdata.ans + "', 256))));";
+            var query = "insert into players values ('"+ qdata.name + "', '" + qdata.email + "', '" + qdata.user + "', aes_encrypt('" + qdata.pass + "', unhex(sha2('" + qdata.pass + "', 256))), '" + qdata.ques + "', aes_encrypt('ans', unhex(sha2('" + qdata.ans + "', 256))));";
             con.query(query, function(err, result){
                 if(err) throw err;
                 console.log("added data successfully");
