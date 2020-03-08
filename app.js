dadb0655d9a6e1444887524c558f987cd832fd8f
bhch1507 <56021613+bhch1507@users.noreply.github.com>
2020-03-08 15:36:14 +0530

    added navbar url functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname+'/public'));
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "bhch@1507",
   database: 'Forum',
 //   port: "3306",
 //   insecureAuterh : true,
