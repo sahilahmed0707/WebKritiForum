01176193738860af7050ffe70163d4330f278bd0
bhch1507 <56021613+bhch1507@users.noreply.github.com>
2020-03-12 14:58:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -253,5 +253,5 @@ var server = app.listen(8080, function () {
   var host = server.address().address
   var port = server.address().port
   
-  console.log("Example app listening at http://%s:%s", host, port)
+  console.log("Server started at", host, port)
 });
